4ef135867a14437b9998d8694cba28245abdc48d
Gholam Hosin <142669071+gholam4030@users.noreply.github.com>
2023-11-25 07:58:11 +0100

    This is the Quadratic equation with three possible way, if D>0 means it has two real root, if D==0 then it has one real root and if D<0 then it has not any real root.

--- Quadratic.py
+++ Quadratic.py
@@ -1,24 +1,61 @@
-import math
+from math import sqrt
 
-def solve_quadratic(a, b, c):
+def solve_quadratic(a: float, b: float, c: float):
+
+    #این ها فقط تشریحات است برای کسی که کود شما را میخوانند
+    """
+    Calculate the roots of a quadratic equation.
+
+    Parameters:
+    - a (float): Coefficient of x^2.
+    - b (float): Coefficient of x.
+    - c (float): Constant term.
+
+    Returns:
+    List: A list containing the roots or None if D < 0.
+    """
+    # اخر تشریحات
     # Calculate the discriminant
-    D = b**2 - 4*a*c
+    discriminant = b**2 - 4*a*c
 
-    # Check the discriminant
-    if D > 0:
-        # Two real and distinct solutions
-        x1 = (-b + math.sqrt(D)) / (2*a)
-        x2 = (-b - math.sqrt(D)) / (2*a)
-        return [x1, x2]
-    elif D == 0:
-        # One real solution
-        x = -b / (2*a)
-        return [x]
+    # Check the discriminant and calculate solutions accordingly
+    if discriminant > 0:
+        # Two real roots
+        root1 = (-b + sqrt(discriminant)) / (2*a)
+        root2 = (-b - sqrt(discriminant)) / (2*a)
+        return [root1, root2]
+    elif discriminant == 0:
+        # One real root
+        root = -b / (2*a)
+        return [root]
     else:
-        # No real solution
+        # No real roots
         return None
 
-# Tests
-print(solve_quadratic(56, 67, 2))  # D > 0, Two solutions: [2.0, 1.0]
-print(solve_quadratic(1, 34, 1))  # D = 0, One solution: [1.0]
-print(solve_quadratic(1, 2, 1))   # D < 0, No real solution: None
+
+
+# Test cases
+test_cases = [
+    # Two real roots
+    ((1, -3, 2), [2.0, 1.0]),
+
+    # One real root
+    ((1, -2, 1), [1.0]),
+
+    # No real roots (discriminant < 0)
+    ((1, 2, 5), None),
+]
+
+# Validate the results for each test case
+for i, (coefficients, expected_result) in enumerate(test_cases, start=1):
+    result = solve_quadratic(*coefficients)
+
+    print(f"\nTest Case {i}:")
+    print(f"Coefficients: {coefficients}")
+    print("Expected Result:", expected_result)
+    print("Actual Result:", result)
+
+    assert result == expected_result
+
+# If all assertions pass, print success message
+print("\nAll test cases passed successfully!")
