3c57743005bfa6d0d71bf90863b4d97ffb9f6e10
Gholam Hosin <142669071+gholam4030@users.noreply.github.com>
2024-02-06 19:55:18 +0100

    updated

--- python_exercise/triangle_check.py
+++ python_exercise/triangle_check.py
@@ -1,12 +1,16 @@
-def triangle_fun(x,y,z):
-    if x==y==z:
+def triangle_fun(x, y, z):
+    if x <= 0 or y <= 0 or z <= 0:
+        print('Invalid input')
+    elif x + y <= z or x + z <= y or y + z <= x:
+        print('Invalid input')
+    elif x == y == z:
         print('This is equilateral triangle')
-    elif x==y or x==z or y==z:
+    elif x == y or x == z or y == z:
         print('This is isosceles triangle')
     else:
         print('This is a scalene')
 
-a=int(input('Type first edge'))
-b=int(input('Type second edge'))
-c=int(input('Type third edge'))
-triangle_fun(a,b,c)
+a = int(input('Type first edge: '))
+b = int(input('Type second edge: '))
+c = int(input('Type third edge: '))
+triangle_fun(a, b, c)
