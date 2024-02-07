c5fe5b041f77b323655da54286754577c1c8edd7
Nikolai <nikdenof@gmail.com>
2024-02-07 09:53:22 +0100

    Added the exercise to do

--- python_exercise/triangle_check.py
+++ python_exercise/triangle_check.py
@@ -1,16 +1,24 @@
-def triangle_fun(x, y, z):
-    if x <= 0 or y <= 0 or z <= 0:
-        print('Invalid input')
-    elif x + y <= z or x + z <= y or y + z <= x:
-        print('Invalid input')
-    elif x == y == z:
-        print('This is equilateral triangle')
-    elif x == y or x == z or y == z:
-        print('This is isosceles triangle')
-    else:
-        print('This is a scalene')
+def classify_triangle(x: int, y: int, z: int) -> str:
+    # If input is valid, then the code goes further to check other conditions
+    if not is_input_valid(x, y, z):  # if NOT valid, return string
+        return "Invalid input"  # When return no other code is executed, exit function
+    if x == y == z:
+        return "This is an equilateral triangle"  # When return no other code is executed, exit function
+    if x == y or x == z or y == z:
+        return "This is an isosceles triangle"  # When return no other code is executed, exit function
+    return "This is a scalene triangle"  # Equivalent to else as there are no other options left
 
-a = int(input('Type first edge: '))
-b = int(input('Type second edge: '))
-c = int(input('Type third edge: '))
-triangle_fun(a, b, c)
+
+def is_input_valid(x: int, y: int, z: int) -> bool:
+    if x <= 0 or y <= 0 or z <= 0:
+        return False
+    # Check the Triangle Inequality Theorem, returns bool (True or False)
+    return x + y > z and x + z > y and y + z > x  # This will be True or False
+
+
+if __name__ == "__main__":
+    a = int(input("Type first edge: "))
+    b = int(input("Type second edge: "))
+    c = int(input("Type third edge: "))
+    return_string = classify_triangle(a, b, c)
+    print(return_string)
